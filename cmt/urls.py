0ec0b5267e4faf8baaa0838052604b5193530062
YashviChhatbar <chhatbar.yashvi@gmail.com>
2024-12-26 21:12:28 +0530

    init

--- cmt/urls.py
+++ cmt/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from cmt import  views
 
 urlpatterns = [
-    # path("", views.index, name = 'home'),
+    path("", views.index, name = 'home'),
     path("system-info/", views.system_info, name='system_info'),
 ]
